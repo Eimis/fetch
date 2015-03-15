67996e78db5a22889d241b0fc85c47ff2b0d3976
Eimantas <eimantas.stonys@gmail.com>
2015-03-15 23:29:52 +0200

    Dashboard page design
    Initial commit

--- fetch/urls.py
+++ fetch/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns(
     '',
     url(r'^hello/$', 'fetch.views.hello', name='hello'),
+    url(r'^dashboard/$', 'fetch.views.dashboard', name='dashboard'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- fetch/views.py
+++ fetch/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def hello(request):
     return render(request, 'hello.html', {})
+
+
+def dashboard(request):
+    return render(request, 'dashboard.html', {})
