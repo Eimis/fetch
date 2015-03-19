202a479fa6f06404ba3c227611066aead9f47371
Eimantas <eimantas.stonys@gmail.com>
2015-03-19 22:10:37 +0200

    Setup landing page redirection

--- fetch/urls.py
+++ fetch/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns(
     '',
+    url(r'^$', 'fetch.views.hello', name='hello'),
     url(r'^hello/$', 'fetch.views.hello', name='hello'),
     url(r'^dashboard/$', 'fetch.views.dashboard', name='dashboard'),
     url(r'^admin/', include(admin.site.urls)),
